c3e95108dc03bc512d23eed1a22d0bb22bb2e2ef
toryar13 <toryar13@gmail.com>
2016-09-26 16:12:19 +0300

    Working version

--- weather.sh
+++ weather.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
-CITY_URL="http://pogoda.by/26850/"
-curl $CITY_URL 2>/dev/null \
-| sed -nre '/section higher/,/мм рт. ст./p'\
-| sed -r '/section higher|cloudness|wicon wind|crumb|scity|\/div|value m_temp f|m_wind mih|m_wind kmh|png|\/dl class="temp|wicon barp|dt/d'\
-| sed -r 's/(.*)class="type(.*)>(.*)<\/h2>/Город: \3/'\
-| sed -r 's/(.*)<dd(.*)td>(.*)<\/td(.*)\/dd>/Погода: \3/'\
-| sed -r 's/(.*)<dd class=(.*)>(.*)<span class="meas(.*)span><\/dd>/Температура воздуха: \3 C/'\
-| sed -r 's/(.*)value m_wind ms(.*)>(.*)<span class="unit">(.*)<\/span><\/dd>/Ветер: \3 \4/'\
-| sed -r 's/(.*)value m_press torr(.*)>(.*)<(.*)>(.*)<\/span><\/dd>/Давление: \3 \5/'
+#!/bin/sh
+
+TIMEOUT=$(grep "TIMEOUT=" ./config.ini | grep -o "[0-9]*")
+
+while [ true ]
+do
+    rm index.html
+    wget -q	 pogoda.by
+    echo `head -n 297 index.html | tail -n 1 | awk -F "<br>" {'print $2'}`
+    sleep $TIMEOUT
+done
